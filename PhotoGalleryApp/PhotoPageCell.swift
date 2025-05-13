6ef6f9379f2cd84626135a65209986da47085623
AlexPereiraBR <aleksandrshchukinbr@icloud.com>
2025-05-13 17:26:17 -0300

    #WAPP28 - Подробности в Trello.

---
+++ PhotoGalleryApp/PhotoPageCell.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoPageCell.swift
+//  PhotoGalleryApp
+//
+//  Created by Aleksandr Shchukin on 13/05/25.
+//
+
+import Foundation
---
+++ PhotoPageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoPageViewController.swift
+//  PhotoGalleryApp
+//
+//  Created by Aleksandr Shchukin on 13/05/25.
+//
+
+import Foundation
